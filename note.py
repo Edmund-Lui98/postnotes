7a485a491d7f4db5887db7dfc3edb246eccf5b7a
EdmundLuii <edmund.lui1998@gmail.com>
2020-01-30 14:22:03 -0500

    finished note object implementation

--- note.py
+++ note.py
@@ -1,9 +1,27 @@
-class note:
-    def __init__(self):
-        self._msg =
-        self._status = False
-        self._xposition
-        self._yposition
-        self._width
-        self._height
-        self._color
+class Note:
+    def __init__(self, msg, status, xposition, yposition, width, height, color):
+        """
+        -------------------------------------------------------
+        Put note in the server
+        -------------------------------------------------------
+        Parameters:
+            msg - note given by the client (string)
+            status - pinned or unpinned note (boolean)
+            xposition - x coordinate value of the lower left corner (int)
+            yposition - y coordinate value of the lower left corner (int)
+            width - width of the note (int)
+            heigh - height of the note (int)
+            color - color of the note (string)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        self._msg = msg
+        self._status = status
+        self._xposition = xposition
+        self._yposition = yposition
+        self._width = width
+        self._height = height
+        self._color = color
+
+        return
